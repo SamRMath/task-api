c275c914a21d7d3a110ae77b4b8815378d194e64
Sam <smathe17@charlotte.edu>
2025-10-28 20:03:13 -0400

    Fixed getById endpoint error handle logic

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -12,23 +12,37 @@ export async function createTask(req, res, next) {
 }
 
 export async function getTaskById(req, res, next) {
-  const { id } = req.params;
+  try{
+    const { id } = req.params;
 
-  const taskId = Number(id);
-  if (isNaN(taskId)) {
-    return res.status(400).json({
-      error: "Validation failed",
-      details: ["ID must be a number"],
+    const taskId = Number(id);
+    if (isNaN(taskId)) {
+      return res.status(400).json({
+        error: "Validation failed",
+        details: ["ID must be a number"],
+      });
+    }
+
+    const task = await taskService.getTaskById(id);
+
+    if (!task) {
+      return res.status(404).json({
+        error: "Task not found",
+      });
+    }
+
+    return res.json(task);
+  } catch (error) {
+    if (error.code == 'P2023') {
+      return res.status(400).json({
+        error: "Validation failed",
+        details: ["ID must be an integer"],
+      });
+    }
+    console.error(error);
+    return res.status(500).json({
+      error: "Internal server error",
+      message: "Something went wrong",
     });
   }
-
-  const task = await taskService.getTaskById(id);
-
-  if (!task) {
-    return res.status(404).json({
-      error: "Task not found",
-    });
-  }
-
-  return res.json(task);
 }
